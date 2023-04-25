37bc11df8923df1a0334e08009295ed9034e3e82
Renan Fernandes <renzinestuods@gmail.com>
2023-04-25 14:41:09 -0300

    :sparkls: feat: Cria a função study_schedule que retorna o numero de studantes estudando no mesmo horário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    "init"
-    raise NotImplementedError
+    if not type(target_time) == int:
+        return None
+    students = 0
+    for start_time, end_time in permanence_period:
+        if not (type(start_time) == int and type(end_time) == int):
+            return None
+        if target_time >= start_time and target_time <= end_time:
+            students += 1
+
+    return students
