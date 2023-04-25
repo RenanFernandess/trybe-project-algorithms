1abacab5f3e5b06fe1f5c52de389ced9184d6eb2
Renan Fernandes <renzinestuods@gmail.com>
2023-04-25 15:20:38 -0300

    :white_check_mark: test: test_encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
 # from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(message="test", key="")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(message=1, key=3)
+
+    assert encrypt_message(message="message test", key=22) == "tset egassem"
+    assert encrypt_message(message="message test", key=3) == "sem_tset egas"
+    assert encrypt_message(message="message test", key=4) == "tset ega_ssem"
