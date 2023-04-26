58cc65a81b1d1a3687d3f6192444f4f9f9fbfff9
Renan Fernandes <renzinestuods@gmail.com>
2023-04-26 15:04:30 -0300

    :sparkles: feat: Cria a função is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,7 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
-    raise NotImplementedError
+    reversed_word = str()
+    for index in range(len(word) - 1, -1, -1):
+        reversed_word += word[index]
+
+    return (reversed_word == word) and not word == ""
