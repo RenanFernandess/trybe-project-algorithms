6e13ef3f18ff762ce3c6e5196309f4f7032a5f8f
Renan Fernandes <renzinestuods@gmail.com>
2023-04-26 15:27:02 -0300

    :sparkles: feat: Cria a função find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
     """Faça o código aqui."""
-    raise NotImplementedError
+    previous_count = 0
+    count = 0
+    for n in nums:
+        for m in nums:
+            if n == m:
+                count += 1
+        if count > previous_count:
+            previous_count = count
+        count = 0
+
+    return previous_count
